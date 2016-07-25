9d52f0fa220a9e551483e2e5d89319d9d0ac37d3
bhargav <bhargavbhegde7@gmail.com>
2016-07-25 16:19:38 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+var gpio = require("gpio");
+var io = require('socket.io-client');
+var serverUrl = 'http://localhost:8090';
+var conn = io.connect(serverUrl);
+
+var turnLightOn = function(pinNumber){
+    gpioPin = gpio.export(pinNumber,{
+            ready: function(){
+              gpioPin.set();
+              setTimeout(function() {
+                gpioPin.reset();
+              }, 1000 );
+            }
+    });
+  }
+
+conn.emit('join', '{"userID" : "raspberry"}');
+
+conn.on('response',function(data){
+  console.log(data);
+});
+
+conn.on('rasp-message',function(msg){
+  console.log(msg);
+
+  if(msg.indexOf('green') > -1){
+    turnLightOn(18);
+  }
+
+  if(msg.indexOf('red') > -1){
+  	turnLightOn(16);
+  }
+
+  /*if(msg.indexOf('both') > -1){
+    turnLightOn(16);
+  	turnLightOn(18);
+  }*/
+});
