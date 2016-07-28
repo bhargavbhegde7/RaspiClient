671413d27d6aaf3bd6308bd89383d256c3420b92
bhargav <bhargavbhegde7@gmail.com>
2016-07-28 20:43:31 +0530

    url

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var gpio = require("gpio");
 var io = require('socket.io-client');
-var serverUrl = 'http://159.203.161.191:8090';
-//var serverUrl = 'http://localhost:8090/';
+//var serverUrl = 'http://159.203.161.191:8090';
+var serverUrl = 'http://localhost:8090/';
 var conn = io.connect(serverUrl);
 
 var turnLightOn = function(pinNumber){
